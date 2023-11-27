4d434d58d35d2f3f78243bc9ed1b4adade906c30
TheEpikDuck <114428976+TheEpikDuck@users.noreply.github.com>
2023-11-26 21:28:23 -0500

    Update Party.java

--- Party.java
+++ Party.java
@@ -1,24 +1,25 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
 
-public class Party;
+public class Party{
 
 public static void main (String[] args){
-	ArrayList <Attendence> = new ArrayList <Attendence>();
+	
 	
 	int id;
 	String name;
 	String company;
-	// load function? load()
-		//manual, place people
-		
+	int tables;
+	final int numOfTables = 10;
+	
+		//reading the file, though it keeps saying file not found even though its in the same folder
 		try{
 		File people = new File("partyguests.txt");
-		Scanner myReader = new Scanner(myObj);
+		Scanner myReader = new Scanner(people);
 		while (myReader.hasNextLine()) {
 			String arr[] = myReader.nextLine().split(",");
-			System.out.println(data);
+			System.out.println(people);
 		}
 		myReader.close();
      }
@@ -26,5 +27,25 @@ public static void main (String[] args){
 		System.out.println("An error occurred.");
 		e.printStackTrace();
 	}
+	
+	//the part where i try to put people at tables it doesn't work but i'm  not sure how to do it
+	public getTable{
+	Collections.shuffle(name);
+	
+	return tables;
 }
 	
+	
+	Scanner input = new Scanner(new File("partyguests.txt")); //this part is the part where i searches for people or the company
+	while (input.hasNext()){
+		String search = input.next();
+		if(search.equalsIgnoreCase(name)){
+			return name;
+		}
+		else if(search.equalsIgnoreCase(company)){
+			return company;
+		}
+
+	}
+	}
+}	
